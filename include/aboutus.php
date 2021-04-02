723d19ca469845ce599ab0251677c1e3f36ea103
najlaauliad <najlaauliad@users.noreply.github.com>
2021-04-02 11:09:13 +0700

    about us ambil dari konten

--- include/aboutus.php
+++ include/aboutus.php
@@ -1,4 +1,12 @@
-
+<?php
+$sql_k = "SELECT `judul`,`isi` FROM `konten` WHERE
+`id_konten`='3'";
+$query_k = mysqli_query($koneksi, $sql_k);
+while ($data_k = mysqli_fetch_row($query_k)) {
+  $judul_konten = $data_k[0];
+  $isi_konten = $data_k[1];
+}
+?>
     <section id="blog-header">
       <div class="container">
         <h1 class="text-white">ABOUT US</h1>
@@ -7,42 +15,16 @@
     <section id="blog-list">
       <main role="main" class="container">
         <div class="row">
-          <div class="col-md-9 blog-main">
-            <div class="blog-post">
-              <h2 class="blog-post-title">Sample blog post</h2>
-              <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
-
-              <p>This blog post shows a few different types of content that’s supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
-              <hr>
-              <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
-              <blockquote>
-                <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-              </blockquote>
-              <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-              <h2>Heading</h2>
-              <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-              <h3>Sub-heading</h3>
-              <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-              <pre><code>Example code block</code></pre>
-              <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-              <h3>Sub-heading</h3>
-              <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-              <ul>
-                <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
-                <li>Donec id elit non mi porta gravida at eget metus.</li>
-                <li>Nulla vitae elit libero, a pharetra augue.</li>
-              </ul>
-              <p>Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue.</p>
-              <ol>
-                <li>Vestibulum id ligula porta felis euismod semper.</li>
-                <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                <li>Maecenas sed diam eget risus varius blandit sit amet non magna.</li>
-              </ol>
-              <p>Cras mattis consectetur purus sit amet fermentum. Sed posuere consectetur est at lobortis.</p>
-            </div><br><br><!-- /.blog-post -->
-          </div><!-- /.blog-main -->
+        <div class="col-md-7">
+        <h2 class="featurette-heading">
+          <?php echo $judul_konten; ?></span></h2>
+        <p class="lead"><?php echo $isi_konten; ?></p>
+      </div>
+      <div class="col-md-5">
+        <img src="images/undraw_book_lover_mkck.png" class="img-fluid mx-auto featurette-image">
+      </div><!-- /.blog-main -->
       
-          <aside class="col-md-3 blog-sidebar">
+         
       
             <div class="p-4">
               <h4 class="font-italic">Social Media</h4>
