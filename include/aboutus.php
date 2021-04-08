d25fc6c489b9b8f4cc7654005b83cf2ba02645fb
fadilayoga <fadilayoga@users.noreply.github.com>
2021-04-08 09:02:46 +0700

    Update aboutus.php

--- include/aboutus.php
+++ include/aboutus.php
@@ -7,38 +7,38 @@ while ($data_k = mysqli_fetch_row($query_k)) {
   $isi_konten = $data_k[1];
 }
 ?>
-    <section id="blog-header">
-      <div class="container">
-        <h1 class="text-white">ABOUT US</h1>
-      </div>
-    </section><br><br>
+<section id="blog-header">
+  <div class="container">
+    <h1 class="text-white">ABOUT US</h1>
+  </div>
+</section><br><br>
 
-    <section id="blog-list">
-      <main role="main" class="container">
-        <div class="row">
-          <div class="col-md-9 blog-main">
-            <div class="blog-post">
-            <h2 class="blog-post-title">
+<section id="blog-list">
+  <main role="main" class="container">
+    <div class="row">
+      <div class="col-md-9 blog-main">
+        <div class="blog-post">
+          <h2 class="blog-post-title">
             <?php echo $judul_konten; ?></span></h2>
-           <p class="lead"><?php echo $isi_konten; ?></p>
-          </div><br><br><!-- /.blog-post -->
+          <p class="lead"><?php echo $isi_konten; ?></p>
+        </div><br><br><!-- /.blog-post -->
       </div><!-- /.blog-main -->
-      
-      
-         
-      
-            <div class="p-4">
-              <h4 class="font-italic">Social Media</h4>
-              <ol class="list-unstyled">
-                <li><a href="#">GitHub</a></li>
-                <li><a href="#">Twitter</a></li>
-                <li><a href="#">Facebook</a></li>
-                <li><a href="#">Instagram</a></li>
-              </ol>
-            </div>
-          
-      
-        </div><!-- /.row -->
-      
-      </main><!-- /.container -->
-    </section><br><br>
+
+
+
+
+      <div class="p-4">
+        <h4 class="font-italic">Social Media</h4>
+        <ol class="list-unstyled">
+          <li><a href="https://github.com/">GitHub</a></li>
+          <li><a href="https://twitter.com/">Twitter</a></li>
+          <li><a href="https://www.facebook.com/">Facebook</a></li>
+          <li><a href="https://www.instagram.com/">Instagram</a></li>
+        </ol>
+      </div>
+
+
+    </div><!-- /.row -->
+
+  </main><!-- /.container -->
+</section><br><br>
